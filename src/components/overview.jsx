0e56699ef648f01b8f920c0bd1253860e210bf64
reinheimermat <mreinheimer0@gmail.com>
2024-03-24 17:11:49 -0300

    feat: add overview card.

---
+++ src/components/overview.jsx
@@ -0,0 +1,46 @@
+import { GraduationCap, User, Calendar } from 'lucide-react'
+
+export function Overview() {
+  const card = [
+    {
+      title: 'Estudantes',
+      value: 932,
+      color: 'bg-primary',
+      icon: <GraduationCap className="size-10" />,
+    },
+    {
+      title: 'Professores',
+      value: 754,
+      color: 'bg-orange-600',
+      icon: <User className="size-10" />,
+    },
+    {
+      title: 'Turmas',
+      value: 40,
+      color: 'bg-yellow-400',
+      icon: <Calendar className="size-10" />,
+    },
+  ]
+
+  return (
+    <article className="bg-white mt-20 p-11 rounded-3xl">
+      <ul className="flex gap-4">
+        {card.map((item, index) => (
+          <li key={index} className="flex flex-col gap-2 p-4 rounded-md">
+            <aside className="flex items-center gap-4">
+              <span className={` rounded-full p-4 text-white ${item.color}`}>
+                {item.icon}
+              </span>
+              <div>
+                <h3 className="text-gray-500 text-lg">{item.title}</h3>
+                <p className="text-blue-950 font-bold text-4xl text-center">
+                  {item.value}
+                </p>
+              </div>
+            </aside>
+          </li>
+        ))}
+      </ul>
+    </article>
+  )
+}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,15 +1,12 @@
-import { Link } from 'react-router-dom'
+import { Overview } from '../components/overview'
 
 export function Home() {
   return (
-    <section className="flex items-center justify-between text-end">
-      <h1 className="pt-8 ps-8 text-primary font-bold text-4xl">
+    <section className="ps-12 pt-16">
+      <h1 className=" text-primary font-bold text-4xl">
         Seja bem-vindo (a), Matheus.
       </h1>
-      <Link
-        to="/estudantes"
-        className="bg-primary text-secondary font-bold rounded-full p-2 pe-8 ps-8 text-lg"
-      ></Link>
+      <Overview />
     </section>
   )
 }
